e5f96fec76e2649a74415427d77b4406c7c3e8f8
TaeGo7 <power1894@naver.com>
2017-09-07 15:57:54 +0900

    Update Lineup.c

--- Lineup.c
+++ Lineup.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <Windows.h>
-#pragma warning(disable: 4996)
+//#pragma warning(disable: 4996)
 
-void Lineup();
 void gotoxy();
-void showLineup();
+void WriteLineup();
+void ShowLineup();
 
-struct Bat {
+struct Batter {
 	int AVG;
 	int HIT;
-	int DOUBLE;
-	int TRIPLE;
-	int RUN;
+	/* int DOUBLE;
+	int TRIPLE; 
+	int RUN; */
 	int BB;
 	int SO;
 };
-struct Pit {
+struct Pitcher {
 	int ER;
 	int ERA;
 	int INNING;
@@ -31,18 +31,15 @@ char TeamType[2][5] = { "Home", "Away" };
 char TeamName[2][21];
 char Batter[2][9][21];
 char Pitcher[2][10][21];
-int n[2] = { 0, 25 };
+int n[2] = { 0, 25 }; // Move right
 
 void main()
 {
-	struct Bat B;
-	struct Pit P;
-
-	Lineup();
-	showLineup();
+	WriteLineup();
+	ShowLineup();
 }
 
-void Lineup()
+void WriteLineup()
 {
 	//printf("Write your Baseball Lineup Card\n");
 
@@ -57,9 +54,6 @@ void Lineup()
 		}
 		system("cls");
 	}
-
-
-
 	/*
 	printf("Home Team : ");
 	scanf("%s", TeamName[0]);
@@ -69,7 +63,6 @@ void Lineup()
 	scanf("%s", Batter[0][i]);
 	}
 	system("cls");
-
 	printf("Away Team : ");
 	scanf("%s", TeamName[1]);
 	printf("Batter\n");
@@ -80,14 +73,15 @@ void Lineup()
 	system("cls");
 	*/
 }
-void showLineup()
+void ShowLineup()
 {
 	for (int j = 0; j < 2; j++) {
 		gotoxy(n[j], 0);
-		printf("%s Team\n", TeamType[j]);
-		for (int i = 0; i < 9; i++) {
+		//printf("%s Team\n", TeamType[j]);
+		printf("%s\n", TeamName[j]);
+		for (int i = 1; i < 10; i++) {
 			gotoxy(n[j], i + 1);
-			printf("%s\n", Batter[j][i]);
+			printf("%d. %s\n",i, Batter[j][i - 1]);
 		}
 	}
 }
